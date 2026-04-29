e9ee32023072a15353161b92a3c80d14b2bf4f83
Pr1ThVi26 <prithvijeetsaha@gmail.com>
2026-04-29 08:57:05 +0530

    n nodes

---
+++ single linked list/n nodes.py
@@ -0,0 +1,25 @@
+class Node:
+	def __init__(self,ele):
+		self.data=ele
+		self.next=None
+head,cur,ptr=None,None,None
+c=0
+ct=1
+while ct==1:
+	c=c+1
+	print(f"Enter node {c} data: ")
+	ele=int(input())
+	cur=Node(ele)
+	cur.next=None
+	if head==None:
+		head=cur
+	else:
+		ptr.next=cur
+	ptr=cur
+	print("Do you want to continue adding data(press 1 else press any number to exit): ")
+	ct=int(input())
+print("Elements are: ")
+ptr=head
+while ptr!=None:
+	print(ptr.data)
+	ptr=ptr.next
